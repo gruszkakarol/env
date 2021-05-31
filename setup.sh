be4ace890cc1c604e9acb8bd500b9973e193108d
Sniadekk <karolgruszka9@gmail.com>
2021-05-31 16:06:08 +0200

    feat: rustup and git config

---
+++ setup.sh
@@ -0,0 +1,7 @@
+# Configure Git
+git config --global user.email "karolgruszka9@gmail.com"
+git config --global user.name "Sniadekk"
+
+# Install Rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source $HOME/.cargo/env
