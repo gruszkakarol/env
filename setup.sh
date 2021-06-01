fbe17b6d11b95f8e075d1df8324c9d0216457c1c
Sniadekk <karolgruszka9@gmail.com>
2021-06-01 20:52:46 +0200

    feat: install Rust dependencies

--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
+sudo apt update
 # Configure Git
 git config --global user.email "karolgruszka9@gmail.com"
 git config --global user.name "Sniadekk"
+
+# Install Rust dependencies
+sudo apt install build-essential
 
 # Install Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
